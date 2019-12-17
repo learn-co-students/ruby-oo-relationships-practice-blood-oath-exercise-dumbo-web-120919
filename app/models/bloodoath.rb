44702a48b3f19565cac8efd5bf06521e101b7fdc
Annie Liao <liaowow@gmail.com>
2019-12-17 08:43:18 -0500

    Done.

--- app/models/bloodoath.rb
+++ app/models/bloodoath.rb
@@ -1,11 +1,23 @@
 class BloodOath
 
-    attr_reader :initiation_date
+    attr_reader :initiation_date, :cult, :follower
     @@all = []
 
-    def initialize(initiation_date)
+    def initialize(initiation_date, cult, follower)
         @initiation_date = initiation_date
+        @cult = cult
+        @follower = follower
         @@all << self
     end
 
+    def self.first_oath
+        first_oath = @@all.min_by do |bloodoath_instance|
+            bloodoath_instance.initiation_date
+        end
+        first_oath.follower
+    end
+
+    def self.all
+        @@all
+    end
 end
--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,5 +1,4 @@
 class Follower
-    attr_accessor :cults
     attr_reader :name, :age, :life_motto
 
     @@all = []
@@ -8,8 +7,52 @@ class Follower
         @name = name
         @age = age
         @life_motto = life_motto
-        @cults = []
+
         @@all << self
+    end
+
+    def join_cult(cult)
+        if self.age >= cult.minimum_age
+            BloodOath.new("2019-12-16", cult, self)
+        else
+            puts "You're too young for this cult!"
+        end
+    end
+
+    # helper method: find all the bloodoaths for this follower
+    def blood_oaths
+        BloodOath.all.select do |bloodoath_instance|
+            bloodoath_instance.follower == self
+        end
+    end
+    
+    def cults
+        self.blood_oaths.map(&:cult)
+        # the above line is same as:
+        # self.blood_oaths.map{|bloodoath_instance| bloodoath_instance.cult} 
+    end
+
+    def my_cults_slogans
+        self.cults.each do |cult|
+            puts cult.slogan
+        end
+    end
+
+    def self.most_active
+        @@all.max_by do |follower|
+            follower.cult.count
+        end
+    end
+
+    def self.top_ten
+        # sort followers by num of cults they joined
+        # sort_by will sort from lowest to highest
+        sorted_followers = self.all.sort_by do |follower|
+            follower.cults.count
+        end
+        # sort from highest to lowest
+        sorted_followers = sorted_followers.reverse
+        sorted_followers[0..9]
     end
 
     def self.of_a_certain_age(age)
